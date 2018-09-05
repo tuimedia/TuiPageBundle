a95012f7a953a67d3e61f79f5add3afb241886a7
Matt Robinson <matt@tuimedia.com>
2018-09-05 10:13:10 +0100

    Create element

--- Controller/ElementController.php
+++ Controller/ElementController.php
@@ -5,7 +5,9 @@ namespace Tui\PageBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Tui\PageBundle\Repository\ElementRepository;
+use Tui\PageBundle\Entity;
 
 class ElementController extends AbstractController
 {
@@ -18,6 +20,22 @@ class ElementController extends AbstractController
 
         return $this->json($elements, 200, [], [
             'groups' => ['elementList'],
+        ]);
+    }
+
+    /**
+     * @Route("/elements", methods={"POST"}, name="tui_page_element_create")
+     */
+    public function create(Request $request, SerializerInterface $serializer, ElementRepository $elementRepository)
+    {
+        $element = $serializer->deserialize($request->getContent(), Entity\Element::class, 'json', [
+            'groups' => ['elementCreate'],
+        ]);
+
+        $elementRepository->save($element);
+
+        return $this->json($element, 200, [], [
+            'groups' => ['elementGet'],
         ]);
     }
 }
