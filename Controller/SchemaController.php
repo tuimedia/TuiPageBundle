d393d90e2eca7657acf7caa83c88ee02f370816b
Matt Robinson <matt@matt-mac.studio.tuimedia.com>
2018-09-26 16:30:04 +0100

    Endpoint to return component schemas

--- Controller/SchemaController.php
+++ Controller/SchemaController.php
@@ -30,4 +30,26 @@ class SchemaController extends AbstractController
 
         return $response;
     }
+
+    /**
+     * @Route("/pages/schema/{component}.schema.json", methods={"GET"}, name="tui_page_schema_component")
+     */
+    public function component(RouterInterface $router, $componentSchemas, string $component)
+    {
+        if (!isset($componentSchemas[$component])) {
+            throw $this->generateNotFoundException();
+        }
+
+        // Load the component file and replace the $id URL with a pointer to this location
+        $schema = file_get_contents($componentSchemas[$component]);
+        $correctId = $router->generate('tui_page_schema_component', ['component' => $component], UrlGeneratorInterface::ABSOLUTE_URL);
+        $jsonId = json_encode($correctId, JSON_UNESCAPED_SLASHES);
+
+        $schema = preg_replace('/"\$id":\s+"([^\"]+)"/', sprintf('"$id": %s', $jsonId), $schema);
+
+        $response = new Response($schema);
+        $response->headers->set('Content-Type', 'application/schema+json');
+
+        return $response;
+    }
 }
